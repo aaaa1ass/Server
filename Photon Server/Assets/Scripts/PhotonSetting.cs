2fc232068464f1aba86934876e6090d1f4afa5ee
aaaa1ass <aaaa1ass@gmail.com>
2023-11-09 21:47:53 +0900

    2023/11/09
    photon playfab

---
+++ Photon Server/Assets/Scripts/PhotonSetting.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class PhotonSetting : MonoBehaviourPunCallbacks
+{
+    [SerializeField] InputField email;
+    [SerializeField] InputField password;
+    [SerializeField] InputField userID;
+
+
+}

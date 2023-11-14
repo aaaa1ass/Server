ab54e0980ee8f62f9d617bf8b25208a416cdf530
aaaa1ass <aaaa1ass@gmail.com>
2023-11-14 21:46:38 +0900

    2023/11/14
    Photon Game

--- Photon Server/Assets/Scripts/RoomManager.cs
+++ Photon Server/Assets/Scripts/RoomManager.cs
@@ -45,4 +45,57 @@ public class RoomManager : MonoBehaviourPunCallbacks
             room.GetComponent<Information>().SetInfo(info.Name, info.PlayerCount, info.MaxPlayers);
         }
     }
+
+    public void OnclickCreateRoom() 
+    { 
+        //룸 옵션 설정
+        RoomOptions Room = new RoomOptions();
+
+        //최대 접속자 수
+        Room.MaxPlayers = byte.Parse(roomPerson.text);
+
+        //룸 오픈 여부
+        Room.IsOpen = true;
+
+        //룸 목록 노출 여부
+        Room.IsVisible = true;
+
+        //룸 생성
+        PhotonNetwork.CreateRoom(roomName.text, Room);
+    }
+
+    public void AllDeleteRoom()
+    {
+        //Transform 오브젝트에 있는 하위 오브젝트에 접근하여 전체 삭제
+        foreach (Transform trans in roomContent)
+        {
+            Destroy(trans.gameObject);
+        }
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        AllDeleteRoom();
+        UpdateRoom(roomList);
+        CreateRoomObject();
+    }
+
+    private void UpdateRoom(List<RoomInfo> roomList)
+    {
+        for(int i = 0; i < roomList.Count; i++)
+        {
+            if (roomCatalog.ContainsKey(roomList[i].Name))
+            {
+                //RemovedFromList 룸에서 삭제 여부
+                if (roomList[i].RemovedFromList)
+                {
+                    roomCatalog.Remove(roomList[i].Name);
+                    continue;
+                }
+            }
+            roomCatalog[roomList[i].Name] = roomList[i];
+        }
+
+        
+    }
 }

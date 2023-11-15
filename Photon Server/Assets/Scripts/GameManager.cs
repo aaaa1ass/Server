e6d3629be67bdbb4b159a52b198c06677111064b
aaaa1ass <aaaa1ass@gmail.com>
2023-11-15 21:46:29 +0900

    2023/11/15
    동시 접속 시스템 추가

--- Photon Server/Assets/Scripts/GameManager.cs
+++ Photon Server/Assets/Scripts/GameManager.cs
@@ -1,18 +1,70 @@
+using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject preasureBox;
+
+    private void Awake()
     {
-        
+        PhotonNetwork.Instantiate
+            (
+                "Character",
+                RandomPosition(5),
+                Quaternion.identity
+            );
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        if(PhotonNetwork.IsMasterClient)
+        {
+            StartCoroutine(CreateObject());
+        }
+    }
+
+    public Vector3 RandomPosition(int value)
+    {
+        Vector3 direction = Random.insideUnitSphere;
+
+        direction *= value;
+
+        direction.y = 0;
+
+        return direction;
+    }
+
+    private IEnumerator CreateObject()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5f);
+
+            PhotonNetwork.Instantiate
+                (
+                    "Treasure Box",
+                    RandomPosition(25),
+                    Quaternion.identity
+                );
+        }
+
+    }
+
+    public void ExitGame()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnLeftRoom()
+    {
+        PhotonNetwork.LoadLevel("Photon Room");
+    }
+
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        PhotonNetwork.SetMasterClient(PhotonNetwork.PlayerList[0]);
     }
 }
